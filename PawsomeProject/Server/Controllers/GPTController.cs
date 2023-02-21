46839eb5cab931c5fe26a73a65164b0f16e2761d
Pat <la_spirou@hotmail.com>
2023-02-21 10:09:40 -0500

    adoption animal page

--- PawsomeProject/Server/Controllers/GPTController.cs
+++ PawsomeProject/Server/Controllers/GPTController.cs
@@ -1,67 +1,36 @@
-<<<<<<< HEAD
-﻿//using Microsoft.AspNetCore.Http;
-//using Microsoft.AspNetCore.Mvc;
-//using OpenAI_API;
-//using OpenAI_API.Completions;
 
-//namespace PawsomeProject.Server.Controllers
-//{
-//    [Route("api/[controller]")]
-//    [ApiController]
-//    public class GPTController : ControllerBase
-//    {
-//        [HttpGet]
-//        [Route("UseChatGPT")]
-//        public async Task<IActionResult> UseChatGPT(string query) 
-//        {
-//            string OutPutResult = "";
-//            var openai = new OpenAIAPI("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");
-//            CompletionRequest completionRequest = new CompletionRequest();
-//            completionRequest.Prompt = query;
-//            completionRequest.Model = OpenAI_API.Models.Model.DavinciText;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OpenAI_API;
+using OpenAI_API.Completions;
 
-//            var completions = openai.Completions.CreateCompletionAsync(completionRequest);
+namespace PawsomeProject.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GPTController : ControllerBase
+    {
+        [HttpGet]
+        [Route("UseChatGPT")]
+        public async Task<IActionResult> UseChatGPT(string query)
+        {
+            string OutPutResult = "";
+            var openai = new OpenAIAPI("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");
+            CompletionRequest completionRequest = new CompletionRequest();
+            completionRequest.Prompt = query;
+            completionRequest.Model = OpenAI_API.Models.Model.DavinciText;
 
-//            foreach (var completion in completions.Result.Completions)
-//            {
-//                OutPutResult += completion.Text;
-//            }
+            var completions = openai.Completions.CreateCompletionAsync(completionRequest);
 
-//            return Ok(OutPutResult);
-//        }
-//    }
-//}
-=======
-﻿// using Microsoft.AspNetCore.Http;
-// using Microsoft.AspNetCore.Mvc;
-// using OpenAI_API;
-// using OpenAI_API.Completions;
-//
-// namespace PawsomeProject.Server.Controllers
-// {
-//     [Route("api/[controller]")]
-//     [ApiController]
-//     public class GPTController : ControllerBase
-//     {
-//         [HttpGet]
-//         [Route("UseChatGPT")]
-//         public async Task<IActionResult> UseChatGPT(string query) 
-//         {
-//             string OutPutResult = "";
-//             var openai = new OpenAIAPI("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");
-//             CompletionRequest completionRequest = new CompletionRequest();
-//             completionRequest.Prompt = query;
-//             completionRequest.Model = OpenAI_API.Models.Model.DavinciText;
-//
-//             var completions = openai.Completions.CreateCompletionAsync(completionRequest);
-//
-//             foreach (var completion in completions.Result.Completions)
-//             {
-//                 OutPutResult += completion.Text;
-//             }
-//
-//             return Ok(OutPutResult);
-//         }
-//     }
-// }
->>>>>>> 81f089b0352ae90223b7789408da23c9fb7dd774
+            foreach (var completion in completions.Result.Completions)
+            {
+                OutPutResult += completion.Text;
+            }
+
+            return Ok(OutPutResult);
+        }
+    }
+}
+
+
+
