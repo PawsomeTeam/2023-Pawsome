f48158ecf4f89d984a79b2625bc75a8fb3ea11f4
juliatag <julieta.jabbot@gmail.com>
2023-02-23 03:55:30 -0500

    PAW-69 Admin and Public navigation adjustments and additions

--- PawsomeProject/Shared/Models/FindUser.cs
+++ PawsomeProject/Shared/Models/FindUser.cs
@@ -3,12 +3,12 @@ namespace PawsomeProject.Shared.Models;
 public class FindUser
 {
     public string UserName { get; set; }
-    
+
     public string FirstName { get; set; }
-    
+
     public string LastName { get; set; }
-    
+
     public string Email { get; set; }
-    
+
     public string? PhoneNumber { get; set; }
 }
